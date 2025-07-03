7ea1e45f76fdfae8974e7a0b240f547e3c3f878e
9119sk <khansameer44801@gmail.com>
2025-07-03 23:20:52 +0530

    chore: update backend dependencies to include body-parser, cors, dotenv, express, mongoose, and socket.io

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const yargs = require('yargs');
 const { hideBin } = require('yargs/helpers');
 const initRepo = require('./controller/init');
@@ -7,10 +8,19 @@ const { push } = require('./controller/push');
 const { revert } = require('./controller/revert');
 const { pull } = require('./controller/pull');
 const { argv } = require('process');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser')
+const http = require('http')
+const mongoose = require('mongoose');
+const { Server } = require("socket.io");
+const { Socket } = require('dgram');
 
 
 yargs(hideBin(process.argv))
-    .command("init", "initialize a new repository",{}, initRepo)
+    .command('start', "start a server", () => { startServer() })
+    .command("init", "initialize a new repository", {}, initRepo)
     .command("add <file>", "add a file to the repository", (yargs) => {
         yargs.positional("file", {
             describe: " file to add",
@@ -18,7 +28,7 @@ yargs(hideBin(process.argv))
 
         }
         )
-    }, (argv)=>{
+    }, (argv) => {
         add(argv.file);
     })
 
@@ -27,10 +37,10 @@ yargs(hideBin(process.argv))
             describe: "commit message",
             type: "string",
         })
-  
-    }, (argv)=>{commit(argv.message)} )
 
-    .command("push", "push changes to the remote repository", {},push)
+    }, (argv) => { commit(argv.message) })
+
+    .command("push", "push changes to the remote repository", {}, push)
 
     .command("revert <commitId>", "revert changes to a specific commit", (yargs) => {
         yargs.positional("commitId", {
@@ -38,10 +48,71 @@ yargs(hideBin(process.argv))
             type: "string",
         })
 
-    },(argv)=> {revert(argv.commitId)})
+    }, (argv) => { revert(argv.commitId) })
 
-    .command("pull", "pull changes from the remote repository", {}, ()=>{pull()})
+    .command("pull", "pull changes from the remote repository", {}, () => { pull() })
 
     .demandCommand(1, "you need at least one command before moving on").help().argv;
 
 
+
+
+async function startServer() {
+    try {
+
+        const Port = process.env.PORT || 3000
+        const url = process.env.MONGODB_URI;
+
+        app.use(bodyParser.json());
+        app.use(express.json());
+        app.use(cors({ origin: '*' }))
+
+
+
+        await mongoose.connect(url).then(() => {
+            console.error("connection successful mgd")
+        }).catch((err) => { console.log(err) })
+
+
+
+
+
+        const httpServer = http.createServer(app);
+        const io = new Server(httpServer, {
+            cors: {
+                origin: '*',
+                methods: ['GET', 'POST']
+            }
+        })
+
+
+        io.on('connection', (socket) => {
+            socket.on("joinRoom", (userId) => {
+                let user = userId;
+                console.log("====")
+                console.log(user)
+                console.log("====");
+                socket.join(userId)
+
+            });
+        })
+        const db = mongoose.connection;
+        db.once("open", () => {
+            console.log("MongoDB connection is open");
+            // your logic here
+        });
+
+        httpServer.listen(Port, () => {
+            console.log('Server is listening on port', Port);
+        });
+
+
+        app.get('/', (req, res) => {
+            res.send("hello")
+        });
+
+
+    } catch (err) {
+        console.error('Failed to start server:', err);
+    }
+}
