67e7abc0a6dea8f747d319dd33d4cac81c358151
9199sk <khansameer44801@gmail.com>
2025-07-18 00:44:58 +0530

    setup issues model api

--- backend/controller/issueCont.js
+++ backend/controller/issueCont.js
@@ -1,28 +1,98 @@
-const createIssue= (req, res) => {
-    res.send("issue controller")
+const { describe } = require("yargs");
+const Issue = require("../model/issue")
+
+const createIssue = async (req, res) => {
+    const { title, description } = req.body;
+    const repoId = req.params;
+    try {
+        const issue = new Issue({
+            title,
+            description,
+            repository: repoId
+        })
+
+        await issue.save();
+        res.status(201).json(issue)
+    }
+    catch (err) {
+        console.log("error during create issue", err);
+        res.status(500).send("server error")
+    }
 }
 
-const deleteIssue= (req, res) => {
-    res.send("delete issue controller")
+const deleteIssueById = async (req, res) => {
+    const { id } = req.params
+    try {
+        const issue = Issue.findByIdAndDelete(id);
+        res.send("issue deleted successfully", issue)
+
+    }
+    catch (err) {
+        console.log("error during delete issue", err);
+        res.status(500).send("server error")
+    }
 }
 
-const updateIssue= (req, res) => {
-    res.send("update issue controller")
+
+const updateIssueById = async (req, res) => {
+    const { id } = req.params;
+    const { title, description, open } = req.body
+
+    try {
+        const issue = await Issue.findById(id);
+        if (!issue) {
+            return res.send("issue not found")
+        }
+        issue.title = title;
+        issue.description = description;
+        issue.status = open;
+
+        await issue.save();
+        res.send("issue updated successfully")
+
+    }
+    catch (err) {
+        console.log("error during updating issue", err);
+        res.status(500).send("server error")
+    }
 }
 
-const fetchIssueById= (req, res) => {
-    res.send("fetch issue by id controller")
+const fetchIssueById = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const issue = await Issue.findById(id).populate('repository');
+        if (!issue) {
+           return res.status(400).json("not found any issue")
+        }
+        res.status(201).json(issue)
+
+    }
+    catch (err) {
+        console.log("error during fetch issue", err);
+        res.status(500).send("server error")
+    }
 }
 
-const fetchAllIssues= (req, res) => {
-    res.send("fetch all issues controller")
+const fetchAllIssues = async (req, res) => {
+    try {
+        const allIssue = await Issue.find({});
+        if (!allIssue) {
+            return res.send("no issue found")
+        }
+        res.send({ message: 'all issue', allIssue })
+
+    }
+    catch (err) {
+        console.log("error during fetching all issue", err);
+        res.status(500).send("server error")
+    }
 }
 
 
 module.exports = {
     createIssue,
-    deleteIssue,
-    updateIssue,
+    deleteIssueById,
+    updateIssueById,
     fetchIssueById,
     fetchAllIssues
 }
